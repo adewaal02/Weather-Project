633ad9ffca66e9d818eff165eb5ef3197f41940a
adewaal02 <aneld58@gmail.com>
2023-05-16 09:21:50 +0200

    Updated API Integration

--- src/script.js
+++ src/script.js
@@ -20,7 +20,6 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
@@ -132,15 +131,6 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-
-
-function getForecast(coordinates) {
-  console.log(coordinates);
-  //let apiKey = "9cb245c8974a9aa2bee9c6e33954b52a";
-  //let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  //console.log(apiUrl);
-  //axios.get(apiUrl);
-}
 let celsiusTemperature = null;
 
 function displayTemperature(response) {
@@ -164,11 +154,9 @@ function displayTemperature(response) {
   iconElement.setAttribute("src", response.data.condition.icon_url);
   iconElement.setAttribute("alt", response.data.condition.description);
 
-  getForecast(response.data.coord);
+  getForecast(response.data.coordinates);
 }
 search("Namibia");
-
-displayForecast(); 
 
 let form = document.querySelector("#form");
 form.addEventListener("submit", handleSubmit);
